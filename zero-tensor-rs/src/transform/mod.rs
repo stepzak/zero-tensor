8aa69c0c5a6e5b868c1d39bc50b472752f111046
stepzak <stepanzakatin@gmail.com>
2026-08-20 00:17:05 +0300

    add clamp

--- zero-tensor-rs/src/transform/mod.rs
+++ zero-tensor-rs/src/transform/mod.rs
@@ -1,10 +1,13 @@
 pub mod add;
+pub mod clamp;
 pub mod error;
+mod helpers;
 pub mod scale;
 
 use crate::core::dataset::item::TensorViewMut;
 
 pub use add::Add;
+pub use clamp::Clamp;
 pub use error::TransformError;
 pub use scale::Scale;
 
